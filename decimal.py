10783a0250c8fb10298d4a6faca70541bf588528
Ubuntu <ubuntu@ip-172-31-27-73.ec2.internal>
2019-10-25 13:25:42 +0000

    Initial commit

---
+++ decimal.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Jacob Bonner
+# Created on: October 2019
+# This program adds two decimal numbers together (creates floating point)
+
+
+def main():
+    # This function adds two decimal numbers together (creates floating point)
+    print(0.1+0.2)
+
+
+if __name__ == "__main__":
+    main()
